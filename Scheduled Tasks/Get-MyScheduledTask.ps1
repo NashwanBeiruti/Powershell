d48a7d24eaee949b6ba5da2d75ccc52ff48cef70
NashwanBeiruti <38735047+NashwanBeiruti@users.noreply.github.com>
2020-10-10 08:46:22 -0400

    Create Get-MyScheduledTask.ps1

---
+++ Scheduled Tasks/Get-MyScheduledTask.ps1
@@ -0,0 +1,23 @@
+﻿#Requires -Version 4
+
+<#		
+.EXAMPLE
+	PS> Get-MyScheduledTask.ps1 | Where-Object {$_.CreationDate -lt (Get-Date).AddDays(-1)}
+
+	This example will find all scheduled tasks that were created within the last day.
+.EXAMPLE
+	PS> Get-MyScheduledTask.ps1 | Where-Object {($_.Author -notmatch 'Microsoft') -and $_.Author}
+
+	This example will find all scheduled tasks that are non-default.  It removes all scheduled tasks that have an author with
+	the word 'Microsoft' in it or has a null value for the author	
+#>
+[CmdletBinding()]
+[OutputType('Selected.Microsoft.Management.Infrastructure.CimInstance')]
+param ()
+process {
+	try {
+		Get-ScheduledTask | Select-Object TaskName,Author, @{ 'n' = 'CreationDate'; 'e' = { [datetime]$_.Date } }
+	} catch {
+		Write-Error "$($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
+	}
+}
