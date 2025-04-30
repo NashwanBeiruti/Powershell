cfebdfd6b6df33cf69e3ce3db4108e786deaecf9
NashwanBeiruti <38735047+NashwanBeiruti@users.noreply.github.com>
2025-04-30 19:07:00 -0400

    Delete Remove-Disabled-Ad-Computers.ps1

--- ActiveDirectory/Remove-Disabled-Ad-Computers.ps1
+++
@@ -1,5 +0,0 @@
-#Created by :Nash
-$aDisabledAdComputers = Get-ADComputer -Filter * | Where-Object { $_.Enabled -eq $false };
-foreach ($oAccount in $aDisabledAdComputers) {
-	Remove-ADObject -Identity $oAccount -Confirm:$false -Recursive;
-}##endforeach
